ea17bc54442421928391d4a99769f09dce14d4fb
strickerkarin <strickerkarin@hotmail.com>
2021-06-09 20:09:03 -0300

    Corrijo ejercicio FrontendII

--- Frontend II/JS-Selectores-Clases/script.js
+++ Frontend II/JS-Selectores-Clases/script.js
@@ -2,20 +2,4 @@ let modoOscuro = confirm('¿Desde cambiar a modo oscuro?');
 
 if(modoOscuro){
     document.querySelector('body').classList.add('bodyModoOscuro');
-    document.querySelector('h1').classList.add('h1modoOscuro');
-
-    let items = document.querySelectorAll('.item');
-    for(let item of items){
-        item.classList.add('itemModoOscuro')
-    };
-
-    let h2s = document.querySelectorAll('.item h2');
-    for(let h2 of h2s){
-        h2.classList.add('textoModoOscuro');
-    }
-
-    let parrafos = document.querySelectorAll('.item p');
-    for (let parrafo of parrafos) {
-        parrafo.classList.add('textoModoOscuro'); 
-    }     
 }
